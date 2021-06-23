3e83793d28333fdc7077cd5d835796fd33ddd387
Hai Nguyen <david.nguyen@isobar.com>
2021-06-23 23:15:57 +0200

    [plugins][fonts] Add main fonts and font plugins to source and preload them

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-preload-fonts`,
     `gatsby-plugin-image`,
     {
       resolve: `gatsby-plugin-styled-components`,
